1ad42808a6f2406cd18c7cbfb28ab64c18648cf0
Reetika <ritikajibansal@gmail.com>
2023-10-19 02:50:11 -0400

    lab7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route,Link,Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <div className='App' >
+    <nav>
+      <Link to='/' className='Nav-item'> Home </Link>
+      <Link to='/about' className='Nav-item'> About </Link>
+      <Link to='/contact' className='Nav-item'> Contact </Link>
+
+    </nav>
+    <Routes>
+      <Route path='/' exect element={<Home/>}/>
+      <Route path='/about'  element={<About/>}/>
+      <Route path='/contact'  element={<Contact/>}/>
+    </Routes>
     </div>
+  </Router>
   );
 }
 
